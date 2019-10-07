130dc2ee9c20176803edd779c39912f5d47897a1
hengshengl7714 <44928050+hengshengl7714@users.noreply.github.com>
2019-10-07 09:41:19 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let ownerName = "Your Name"
-let userName = "YourUsername";
+let ownerName = "Hengsheng Liu"
+let userName = "hengshengl7714";
 
 document.querySelectorAll(".owner-name").forEach((e) => {
     e.innerHTML = ownerName;
